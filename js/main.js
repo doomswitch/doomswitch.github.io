aaeb1faceb911d6d08e3505236957150b6d0331e
doomswitch <doomswitchgames@gmail.com>
2017-11-20 12:35:51 -0800

    testing bar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-var test = 5;
+function move() {
+    var elem = document.getElementById("myBar"); 
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
