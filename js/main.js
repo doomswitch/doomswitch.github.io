1f607fc0d9932e450eac825ed4c6f581a9b18594
doomswitch <33845350+doomswitch@users.noreply.github.com>
2017-11-21 08:41:42 -0800

    checking in so i can work on this later

--- js/main.js
+++ js/main.js
@@ -1,3 +1,68 @@
+var stats = {};
+var costs = {};
+
+function initializeGame()
+{
+    // product ids:
+    // 0 = invalid
+    // 1 = new account
+    costs[0] = 0;
+    costs[1] = 20;
+
+    stats['cash'] = 20;
+    stats['accountActive'] = false;
+
+    // load save stuff
+
+    if(stats['accountActive'] === true)
+    {
+        var playGameDiv = document.getElementById('playGameContainer');
+        playGameDiv.style.visibility = 'visible';
+    }
+}
+
+function updateStats()
+{
+    var cashDisplay = document.getElementById('statsDisplayCash');
+    cashDisplay.innerText = "$" + stats['cash'];
+
+    if(stats['accountActive'] === true)
+    {
+        var playGameDiv = document.getElementById('playGameContainer');
+        playGameDiv.style.visibility = 'visible';
+    }
+}
+
+function buy(productId)
+{
+    if(productId == 0) { return; }
+    switch(productId)
+    {
+        case 1:
+        {
+            if(stats['cash'] >= 20)
+            {
+                if(stats['accountActive'] === true)
+                {
+                    // dunno what we do here yet
+                    return;
+                }
+                else
+                {
+                    stats['cash'] -= 20;
+                    stats['accountActive'] = true;
+                }
+            }
+            break;
+        }
+        default:
+            break;
+    }
+
+    // always update
+    updateStats();
+}
+
 function beginFill(barName, fillTimeInSeconds)
 {
 	var elem = document.getElementById(barName);
