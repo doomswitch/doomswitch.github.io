9a65b6236081a61521f0c4b6e20711fcce8cf11b
doomswitch <33845350+doomswitch@users.noreply.github.com>
2017-11-21 21:46:26 -0800

    submitting work for the evening

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
 var stats = {};
-var costs = {};
 var progressBarMap = {};
+var buttonMap = {};
 var progressBarTimesInSeconds = {};
 
 function initializeGame()
 {
-    // product ids:
-    // 0 = invalid
-    // 1 = new account
-    costs[0] = 0;
-    costs[1] = 20;
+    stats['costs'] = {};
+    stats['costs']['newAccount'] = 20;
+    stats['costs']['living'] = 500;
 
-    stats['cash'] = 20;
-    stats['accountActive'] = false;
-    stats['totalGamesPlayed'] = 0;
+    stats['misc'] = {};
+    stats['misc']['cash'] = 20;
+    stats['misc']['accountActive'] = false;
+    stats['misc']['totalGamesPlayed'] = 0;
+    stats['misc']['wage'] = 500;
     
     stats['account'] = {};
     stats['account']['gamesPlayed'] = 0;
     stats['account']['gamesWon'] = 0;
     stats['account']['gamesLost'] = 0;
+    stats['account']['mmr'] = 0;
 
-    progressBarMap[0] = "playGameProgressBar";
-    progressBarTimesInSeconds[0] = 1;
+    stats['personal'] = {};
+    stats['personal']['improvementsEnabled'] = false;
+    stats['personal']['lastHitting'] = 0;
+    stats['personal']['skillshots'] = 0;
+    stats['personal']['denying'] = 0;
+    stats['personal']['championPool'] = 0;
+    stats['personal']['itemBuilds'] = 0;
+    stats['personal']['metagame'] = 0;
+
+    stats['theGame'] = {};
+    stats['theGame']['championPool'] = 10;
+
+    progressBarMap['playGame'] = "playGameProgressBar";
+    progressBarTimesInSeconds['playGame'] = 1;
+    buttonMap['playGame'] = "playGameButton";
 
     // load save stuff
 
@@ -31,11 +45,16 @@ function initializeGame()
 function updateStats()
 {
     var cashDisplay = document.getElementById('statsDisplayCash');
-    cashDisplay.innerText = "$" + stats['cash'];
+    cashDisplay.innerText = "$" + stats['misc']['cash'];
 
-    if(stats['accountActive'] === true)
+    if(stats['misc']['accountActive'] === true)
     {
         updateAccountStats();
+    }
+
+    if(stats['personal']['improvementsEnabled'] === true)
+    {
+        updatePersonalStats();
     }
 }
 
@@ -55,6 +74,24 @@ function updateAccountStats()
 
     elem = document.getElementById('statsDisplayGamesLost');
     elem.innerText = stats['account']['gamesLost'];
+
+    elem = document.getElementById('statsDisplayMMR');
+    elem.innerText = stats['account']['mmr'];
+}
+
+function updatePersonalStats()
+{
+    var elem = document.getElementById('chooseFocusRow');
+    elem.style.visibility = 'visible';
+
+    elem = document.getElementById('personalStatsContainer');
+    elem.style.visibility = 'visible';
+    
+    elem = document.getElementById('statsDisplayLastHitting');
+    elem.innerText = stats['personal']['lastHitting'];
+
+    elem = document.getElementById('statsDisplaySkillshots');
+    elem.innerText = stats['personal']['skillshots'];
 }
 
 function buy(productId)
@@ -62,19 +99,20 @@ function buy(productId)
     if(productId == 0) { return; }
     switch(productId)
     {
-        case 1:
+        case 1: // new account
         {
-            if(stats['cash'] >= 20)
+            var cost = stats['costs']['newAccount'];
+            if(stats['misc']['cash'] >= cost)
             {
-                if(stats['accountActive'] === true)
+                if(stats['misc']['accountActive'] === true)
                 {
                     // dunno what we do here yet
                     return;
                 }
                 else
                 {
-                    stats['cash'] -= 20;
-                    stats['accountActive'] = true;
+                    stats['misc']['cash'] -= cost;
+                    stats['misc']['accountActive'] = true;
                 }
             }
             break;
@@ -87,11 +125,14 @@ function buy(productId)
     updateStats();
 }
 
-function beginFill(buttonId)
+function beginFill(buttonName)
 {
-	var progressBarElem = document.getElementById(progressBarMap[buttonId]);
+    var button = document.getElementById(buttonMap[buttonName]);
+    button.disabled = true;
+
+	var progressBarElem = document.getElementById(progressBarMap[buttonName]);
     var width = 1;
-    var interval = progressBarTimesInSeconds[buttonId] * 10; // *1000 for ms, /100 for 100 ticks
+    var interval = progressBarTimesInSeconds[buttonName] * 10; // *1000 for ms, /100 for 100 ticks
     var intervalId = setInterval(frame, interval);
     
     function frame()
@@ -99,7 +140,7 @@ function beginFill(buttonId)
     	if(width >= 100) {
         	clearInterval(intervalId);
             width = 1;
-            procComplete(buttonId);
+            procComplete(buttonName);
         } else {
         	width++;
         }
@@ -108,33 +149,65 @@ function beginFill(buttonId)
     }
 }
 
-function procComplete(id)
+function procComplete(buttonName)
 {
-    switch(id) {
-        case 0:
-        {
-            // played a game
-            stats['account']['gamesPlayed']++;
-            stats['totalGamesPlayed']++;
+    if(buttonName == "playGame")
+    {
+        stats['account']['gamesPlayed']++;
+        stats['misc']['totalGamesPlayed']++;
 
-            // win or lose?
-            var winRate = .8;
-            var result = Math.random();
-            if(result > winRate)
-            {
-                stats['account']['gamesWon']++;
-            }
-            else
-            {
-                stats['account']['gamesLost']++;
-            }
-
-            updateStats();
-            break;
-        }
-        default:
+        // win or lose?
+        var winRate = .8;
+        var result = Math.random();
+        if(result > winRate)
         {
-            break;
+            stats['account']['gamesWon']++;
+            stats['account']['mmr'] += 1;
         }
+        else
+        {
+            stats['account']['gamesLost']++;
+            stats['account']['mmr'] -= 2;
+            if(stats['account']['mmr'] < 0) { stats['account']['mmr'] = 0; }
+        }
+
+        if(stats['misc']['totalGamesPlayed'] >= 5 && stats['personal']['improvementsEnabled'] == false)
+        {
+            stats['personal']['improvementsEnabled'] = true;
+        }
+
+        // did we have a focus?
+        var focusButtons = document.getElementsByName("playGameFocus");
+        var selectedFocus = null;
+        var selectedFocusValue = "";
+
+        for(var i = 0; i < focusButtons.length; i++)
+        {
+            if(focusButtons[i].checked)
+            {
+                selectedFocus = focusButtons[i];
+                selectedFocusValue = focusButtons[i].value;
+            }
+        }
+
+        if(stats['personal']['improvementsEnabled'])
+        {
+            if(selectedFocus != null && selectedFocusValue != "")
+            {
+                if(selectedFocusValue == "lastHitting") 
+                { 
+                    stats['personal']['lastHitting']++;
+                }
+                else if(selectedFocusValue == "skillshots")
+                {
+                    stats['personal']['skillshots']++;
+                }
+            }
+        }
+
+        var button = document.getElementById(buttonMap[buttonName]);
+        button.disabled = false;
+
+        updateStats();
     }
 }
